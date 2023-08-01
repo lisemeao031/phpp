8654c52941e6a91b04951b93706f3676caf1151f
lisemeao031 <117774341+lisemeao031@users.noreply.github.com>
2023-08-01 14:55:16 -0300

    Add files via upload

---
+++ meuprimeirositphp/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+$turma2 = "vamos desenvolver website em PHP";
+echo "<br>".$turma2
+$x = 5 /*+ 15 */ + 5;
+echo "<br>". $x;
+
+$txt = "Hello word!";
+$x = 5;
+$y =
+?>
+</body>
+</html>
